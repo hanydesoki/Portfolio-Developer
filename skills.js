779fae00b506960c56e6cd0caac4f60a2d0dae75
Hany Desoki <desoki.hany@live.fr>
2025-08-01 12:25:36 +0200

    first commit

---
+++ skills.js
@@ -0,0 +1,140 @@
+const all_skills = [
+    {
+        key: "python",
+        title: "Python",
+        subtitle: "Programming langage",
+        score: 5,
+        img: "https://cdn-icons-png.flaticon.com/128/5968/5968350.png",
+        text: `
+        <p>
+            Python is my go to langage for almost every project I do. 
+            During my experiences, I learned every aspect of the language
+            by working on all kind of project:
+        </p>
+        <ul>
+            <li>Data Analysis / Data Science with numpy / pandas, pyspark and other</li>
+            <li>Machine Learning with Scikit Lean / Tensorflow</li>
+            <li>Web scrapping</li>
+            <li>Rest API</li>
+            <li>Games</li>
+        </ul>
+        <p>
+            With my experience, I'm able to understand very well how Python work
+            on a lower level:
+        </p>
+        <ul>
+            <li>Decorators</li>
+            <li>Dunder methods</li>
+            <li>Generators</li>
+            <li>Metaclasses</li>
+            <li>Descriptors</li>
+        </ul>
+        <p>
+            By knowing all of this aspect, It is easy for me to quickly learn 
+            new frameworks since most of them are built the same way.
+        </p>
+        `
+    },
+
+    {
+        key: "javascript",
+        title: "Javascript",
+        subtitle: "Programming langage",
+        score: 5,
+        img: "https://cdn-icons-png.flaticon.com/128/5968/5968292.png",
+        text: `
+        <p>
+            During my experience as application engineer, I builted a lot of web app
+            especially dashboards to display KPIs for client.
+        </p>
+        <p>
+            I used it for DOM manipulation (interactivity) but also a lot for
+            data manipulations to format query results and display them.
+        </p>
+        <p>
+            I organise my code to make very modular applications so that we can do significative
+            changes by tweeking some parameters which make appplication easy and fast to develop.
+        </p>
+        <p>
+            A recent exemple is building entire page only by feeding a json object that contains
+            parameters.
+        </p>
+        `
+    },
+    {
+        key: "typescript",
+        title: "Typescript",
+        subtitle: "Programming langage",
+        score: 4,
+        img: "https://cdn-icons-png.flaticon.com/128/5968/5968381.png",
+        text: `
+        <p>
+            Used for ontology api framework.
+        </p>
+        `
+    },
+    {
+        key: "html",
+        title: "HTML",
+        subtitle: "Markup langage",
+        score: 5,
+        img: "https://cdn-icons-png.flaticon.com/128/1051/1051277.png",
+        text: `
+        <p>
+            Mandatory to build my web app squeletons.
+        </p>
+        `
+    },
+    {
+        key: "css",
+        title: "CSS",
+        subtitle: "Style sheet langage",
+        score: 4,
+        img: "https://cdn-icons-png.flaticon.com/128/16020/16020753.png",
+        text: `
+        <p>
+            An application can go from bad to amazing with just good css and nothing more!
+        </p>
+        <p>
+            Some dashboard that I builded as application engineer were done on
+            a low code based platform where we don't have DOM access. So I relied 
+            a lot with css to achieve similar results.
+        </p>
+        `
+    },
+    {
+        key: "react",
+        title: "React JS",
+        subtitle: "Framework",
+        score: 4,
+        img: "https://cdn-icons-png.flaticon.com/128/3459/3459528.png",
+        text: `
+        <p>
+            Used for building app on personal projects.
+        </p>
+        `
+    },
+    {
+        key: "postgresql",
+        title: "PostgreSQL",
+        subtitle: "Database management system",
+        score: 4,
+        img: "https://cdn-icons-png.flaticon.com/128/10464/10464247.png",
+        text: `
+        <p>
+            Used on my dahsboard apps to display kpis.
+        </p>`
+    },
+    {
+        key: "clangage",
+        title: "C Langage",
+        subtitle: "Programming Langage",
+        score: 3,
+        img: "https://cdn-icons-png.flaticon.com/128/3665/3665899.png",
+        text: `
+        <p>
+            Used it on personal project to get some basics from a lower level langage.
+        </p>
+        `
+    }
+];
